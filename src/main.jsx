88bf61a2bca1815f23b7fdedf857cda0050c0677
vuvantoann <toanvu1728@gamil.com>
2025-07-31 21:38:21 +0700

    Cấu hình bộ khung cho Redux trong project

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,14 @@
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+import { createStore } from 'redux'
+import allReducers from './reducers/index.jsx'
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById('root')).render(<App />)
+const store = createStore(allReducers)
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
---
+++ src/reducers/index.jsx
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+
+const allReducers = combineReducers({
+  // reducers được nhúng vào đây import fileReducer
+})
+
+export default allReducers
